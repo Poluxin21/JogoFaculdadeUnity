d5b57b5a4c77e9dcdad0bbaf62a9ef6d03609b55
Guilherme Bento da Silva <guilhermebentodasilva55@gmail.com>
2025-06-07 14:04:43 -0300

    Initial commit

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Horizontal Move")]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float walkspeed = 1;
+    private float xAxis;
+
+    [Header("Ground Check")] [SerializeField]
+    private float jumpForce = 45f;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        Move();
+        Jump();
+    }
+    
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+    
+    private void Move()
+    {
+        rb.linearVelocity = new Vector2(walkspeed * xAxis, rb.linearVelocity.y);
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0),
+                Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0),
+            Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Jump()
+    {
+        
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+        }
+        
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce);
+        }
+    }
+}
